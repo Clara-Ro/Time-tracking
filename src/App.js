502c843bb94191fc66af2d8d339c7927f5736a4c
Clara-Ro <lara.rhee.gree@gmail.com>
2022-02-16 19:26:03 -0300

    html criado

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import React from "react";
+import './Globals.css';
+import './App.css';
+import Tracker from "./components/tracker";
 
 function App() {
   return (
     <>
-      <p>Teste</p>
+      <div className="container">
+
+        <div className="profile">
+          <div className="perfil">
+            <img className="profile-image" src="./assets/images/image-jeremy.png" alt="profile-image" />
+            <span>Report for</span>
+            <strong>Jeremy Robson</strong>
+          </div>
+          <div className="filters">
+            <button className="filter">Daily</button>
+            <button className="filter">Weekly</button>
+            <button className="filter">Monthly</button>
+          </div>
+        </div>
+
+        <Tracker icon='work' title='Work' hour={32} lastWeekHour={36}/>
+        <Tracker icon='work' title='Work' hour={32} lastWeekHour={36}/>
+        <Tracker icon='work' title='Work' hour={32} lastWeekHour={36}/>
+        <Tracker icon='work' title='Work' hour={32} lastWeekHour={36}/>
+        <Tracker icon='work' title='Work' hour={32} lastWeekHour={36}/>
+
+      </div>
     </>
   );
 }
